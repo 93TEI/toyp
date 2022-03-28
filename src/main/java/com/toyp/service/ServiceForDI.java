7d7489865ee0183a50a2c4b37a00f4ee592e0fc3
93TEI <93louis@gmail.com>
2022-03-28 15:57:32 +0900

    @Bean DI + h2 설정

--- src/main/java/com/toyp/service/ServiceForDI.java
+++ src/main/java/com/toyp/service/ServiceForDI.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 @Configuration
+//@Service
 public class ServiceForDI {
 
+    //@Bean은 메소드에서 반환되는 객체를 Bean으로 만들고
+    //@Component는 클래스를 Bean으로 만든다
     @Bean
     public PostsDto createDto(){
         return new PostsDto("service","0325tei","for DI");
