1ff6692d442b1aea10788221ac53f54d5e995851
93TEI <93louis@gmail.com>
2022-04-20 14:56:12 +0900

    Create SecurityConfig & permetAll 부여

---
+++ src/main/java/com/toyp/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.toyp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 다른 곳에서 import하지 않아도 반영됨
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests() // url별 권한 관리 설정의 시작점
+                    .antMatchers("/maze").permitAll();
+    }
+}
