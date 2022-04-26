98d666bebbc1ec9140a4225f2514d16e08382aae
93TEI <93louis@gmail.com>
2022-04-26 11:01:43 +0900

    ID/PW로 접속 -> PW는 암호화 복호화로 검증-> 토큰 생성하여 돌려주기까지 구현 -> (예정) 토큰을 받아 로그인이 지속되기

--- src/main/java/com/toyp/service/LoginService.java
+++ src/main/java/com/toyp/service/LoginService.java
@@ -4,6 +4,8 @@ import com.toyp.config.EnumRole;
 import com.toyp.domain.Posts.Member;
 import com.toyp.domain.Posts.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class LoginService {
     private final MemberRepository mr;
+    // 변수를 만들어서 사용해야 함
+    private final PasswordEncoder pe;
+
+    private final SecurityService ss;
 
     @Transactional
     public String MemberSave(String name, String pw){
@@ -24,8 +30,19 @@ public class LoginService {
 
     // findByEmail로 비번 get해서 검증 -> if문 처리로 아닐 때 exception, 맞을 때 토큰 발행
     @Transactional
-    public String ValidationPassword(String name, String pw){
-        return "s";
+    public String ValidationPassword(String name, String pw) throws UsernameNotFoundException {
+
+        // name 검증
+        String pwFromDB = mr.findByNameMemEquals(name)
+                                .orElseThrow(()-> new UsernameNotFoundException("그런 사용자는 없습니다."))
+                                .getPassword();
+
+        // password 검증
+        if(!pe.matches(pw, pwFromDB)){
+            return new UsernameNotFoundException("비밀번호가 다릅니다").toString(); // throw가 제대로 되는지는 모르겠음
+        } else{
+            return ss.createToken(mr.findByNameMemEquals(name).get().getNameMem(),1000 * 60 *5);
+        }
     }
 
 }
