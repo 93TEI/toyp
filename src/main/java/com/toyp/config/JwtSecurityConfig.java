484f2696b10591e9561994b96f51e8d7df94a6f4
93TEI <43162525+93TEI@users.noreply.github.com>
2022-07-08 18:16:30 +0900

    Create JwtSecurityConfig.java

---
+++ src/main/java/com/toyp/config/JwtSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.toyp.config;
+
+import com.example.jwtLogin.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;
+
+    public void configure(HttpSecurity http){
+        JwtFilter jwtFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
